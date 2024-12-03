fd7c7f2b5d78a9dd0fead0616dcc1517d413cf9d
sleepingpolice-afk <pempekplg3@gmail.com>
2024-12-03 18:17:22 +0700

    buat esun 2

--- pompa.vhd
+++ pompa.vhd
@@ -7,13 +7,18 @@ entity pompa is
         signal clk : in std_logic;
         signal alarm : out std_logic;
         signal pompa : inout std_logic;
-        signal tank_state : in std_logic_vector(1 downto 0);
-        signal luas_pompa : in integer;
+        signal tank_state : in std_logic_vector(1 downto 0); --tracking state tangki
+        signal luas_pompa : in std_logic_vector(2 downto 0);
+        signal kecepatan_air : in std_logic_vector(2 downto 0); --sementara 3 bit
+        signal delivery_head : in std_logic_vector(2 downto 0); --sementara 3 bit
+        signal efisiensi : out std_logic_vector(5 downto 0); --sementara 5 bit
+        signal debit : out std_logic_vector(5 downto 0); --sementara 5 bit
     );
 end entity pompa;
 
 architecture Behavioral of pompa is
     -- variable" disini buat rumus
+    -- daya dan densitas di sini?
 begin
     
 end architecture Behavioral;
