2093b41a311c23cdb731da62b857f0b144c87c05
sleepingpolice-afk <pempekplg3@gmail.com>
2024-12-03 18:04:27 +0700

    hahaha

---
+++ pompa.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity pompa is
+    port (
+        signal clk : in std_logic;
+        
+    );
+end entity pompa;
+
+architecture rtl of pompa is
+    
+begin
+    
+    
+    
+end architecture rtl;
